39f1446be71e538eda479633f8f1674ff04f29f3
Guilherme Flauzino <161965640+guiflauzino18@users.noreply.github.com>
2025-03-07 12:32:23 -0300

    Update command-line.go

--- src/app/command-line.go
+++ src/app/command-line.go
@@ -22,7 +22,7 @@ func Run() *cli.App {
 	app := cli.NewApp()
 	app.Name = "AWS S3 ACTIONS"
 	app.Usage = "Facilitando operações no S3 da Amazon"
-	app.Version = "v1.1.15"
+	app.Version = "v1.1.17"
 
 	app.Commands = []cli.Command{
 		{
