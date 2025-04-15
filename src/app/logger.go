fde3c35d6c05ff2c93af53081abe72c417fc1ad6
guilherme <guilhermegf16@gmail.com>
2025-04-15 16:51:04 -0300

    Dev: Ajuste no timezone dos logs

--- src/app/logger.go
+++ src/app/logger.go
@@ -3,11 +3,25 @@ package app
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
 var LogGlobal = "aws-s3-actions-global"
+
+type timestampHook struct {
+	loc *time.Location
+}
+
+func (h *timestampHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (h *timestampHook) Fire(entry *logrus.Entry) error {
+	entry.Time = entry.Time.In(h.loc)
+	return nil
+}
 
 func ConfiguraLogger(backupProfileName string) (*logrus.Logger, *os.File, error) {
 
@@ -28,10 +42,19 @@ func ConfiguraLogger(backupProfileName string) (*logrus.Logger, *os.File, error)
 		return nil, nil, fmt.Errorf("Erro ao criar arquivo de log: %v", erro)
 	}
 
+	//Carrega fuso horário
+	loc, erro := time.LoadLocation("Local")
+	if erro != nil {
+		return nil, nil, fmt.Errorf("Erro ao carregar timezone: %v", erro)
+	}
+
 	//Configura Logrus
 	logger := logrus.New()
 	logger.SetOutput(logFile)
-	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "02-01-2006 15:04:05",
+	})
+	logger.AddHook(&timestampHook{loc: loc})
 	logger.SetLevel(logrus.InfoLevel)
 
 	return logger, logFile, nil
